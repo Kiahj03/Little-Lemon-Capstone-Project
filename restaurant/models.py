3872afe86fe76f241bb38246cb5f472d815bb986
Kiah Johnson <kiahj@Kiahs-MacBook-Air.local>
2025-09-20 19:34:46 -0400

    Setting up table booking API

--- restaurant/models.py
+++ restaurant/models.py
@@ -7,8 +7,14 @@ class Booking(models.Model):
     no_of_guests = models.IntegerField()
     booking_date = models.DateField()
     
+    def __str__(self):
+        return f"{self.name} @ {self.booking_date}"
+    
 class Menu(models.Model):
     id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     inventory = models.IntegerField()
+    
+    def __str__(self):
+        return self.title
